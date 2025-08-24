d1e506acc73fbbf230bd05af3ea5595e8b997aa3
segaleano <144505287+segaleano@users.noreply.github.com>
2025-08-24 16:30:02 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,6 +19,17 @@ set_val $ICECAST_ADMIN_PASSWORD  admin-password
 set_val $ICECAST_PASSWORD        password
 set_val $ICECAST_HOSTNAME        hostname
 
-set -e
+ # ADD THESE NEW LINES FOR MOUNTPOINT PASSWORDS:
+  if [ -n "$JAZZ_SOURCE_PASSWORD" ]; then
+      echo "Setting Jazz source password"
+      sed -i "s/\${JAZZ_SOURCE_PASSWORD}/$JAZZ_SOURCE_PASSWORD/g" /etc/icecast2/icecast.xml
+  fi
 
-sudo -Eu icecast2 icecast2 -n -c /etc/icecast2/icecast.xml
+  if [ -n "$ROCK_SOURCE_PASSWORD" ]; then
+      echo "Setting Rock source password"
+      sed -i "s/\${ROCK_SOURCE_PASSWORD}/$ROCK_SOURCE_PASSWORD/g" /etc/icecast2/icecast.xml
+  fi
+
+  set -e
+
+  sudo -Eu icecast2 icecast2 -n -c /etc/icecast2/icecast.xml
